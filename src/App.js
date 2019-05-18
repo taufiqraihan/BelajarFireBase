ed3bd8b76a957f822ba1b2d591f8c2fc906bf316
MUHAMMAD TAUFIQ RAIHAN <taufiqraihan9mn@gmail.com>
2019-05-18 11:43:10 +0700

    first Firebase

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import firebase from './firebase/index'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+      super()
+      this.state = {
+        todo: "",
+        todos: []
+      }
+    }
+
+    componentDidMount(){
+
+      firebase.database().ref('todos/').on('value', (snapshot) => {
+        console.log(snapshot.val());
+        const todosFirebase = snapshot.val()
+        const todos = Object.keys(snapshot.val()).map(key => {
+          console.log(todosFirebase[key].todoText)
+          return{
+            key:key,
+            todoText: todosFirebase[key].todoText
+          }
+        })
+        this.setState({todos:todos})
+      })
+    }
+
+    handleChange = (e) => {
+      this.setState({[e.target.name]: e.target.value})
+    }
+
+    handleSubmit = (e) => {
+
+      const newTodoKey = firebase.database().ref('todos/').push().key
+      firebase.database().ref('todos/').update({
+        [newTodoKey]: {
+          todoText: this.state.todo
+        }
+      })
+
+      e.preventDefault()
+    }
+
+    handleRemove = (key) =>{
+      firebase.database().ref(`todos/${key}`).remove()
+    }
+
+    render(){
+      return (
+      <div className="App">
+        <h1>Data Realtime Todo</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            name="todo"
+            onChange={this.handleChange}
+            value={this.state.todo} />
+          <button type="submit">Submit</button>
+        </form>
+        <ul>
+          {
+            this.state.todos.map((data, index) => {
+              return(
+                <li key={index}>
+                  {data.todoText}
+                  <button onClick={(key) => this.handleRemove(data.key)}>Hapus</button>
+                </li>
+              )
+            })
+          }
+        </ul>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/firebase/index.js
@@ -0,0 +1,16 @@
+import * as firebase from 'firebase';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDa59edeUDFla5ZxlQX_F1FgPII771gdwc",
+  authDomain: "fir-sekolah1-2c817.firebaseapp.com",
+  databaseURL: "https://fir-sekolah1-2c817.firebaseio.com",
+  projectId: "fir-sekolah1-2c817",
+  storageBucket: "fir-sekolah1-2c817.appspot.com",
+  messagingSenderId: "613682611132",
+  appId: "1:613682611132:web:8aae3b08adcff34a"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
